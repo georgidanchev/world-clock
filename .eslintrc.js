1f05259e2879229a4f9681e9fd6f8dadf17c3660
Georgi Codes <georgicodes@outlook.com>
2019-03-10 09:47:43 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2016,
+    "ecmaVersion": 2017
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "lines-between-class-members": 1,
+    "no-trailing-spaces": 1,
+    "linebreak-style": 0,
+    "no-unused-vars": 1,
+    "comma-dangle": 2,
+    "eol-last": 0,
+    "no-plusplus": 0,
+    "no-console": 0,
+    "indent": 0,
+  }
+};
---
+++ logic.js
@@ -0,0 +1,70 @@
+class Clock {
+  constructor(target, timeNow, seconds, hour, mins) {
+    this.target = document.querySelector(`${target}`)
+    this.timeNow = timeNow
+    this.seconds = seconds
+    this.hour = hour
+    this.mins = mins
+  }
+
+  createClock() {
+    // Create a blank div element
+    const div = document.createElement('div')
+
+    // Create the clock html structure
+    div.innerHTML = `
+    <div class="clock">
+      <div class="clock-face">
+        <div class="hand hour-hand"></div>
+        <div class="hand min-hand"></div>
+        <div class="hand second-hand"></div>
+      </div>
+    </div>
+    `
+
+    // Get references for the clock hands
+    this.hour = div.querySelector('.min-hand')
+    this.mins = div.querySelector('.hour-hand')
+    this.seconds = div.querySelector('.second-hand')
+
+    // Append the clock
+    this.target.appendChild(div)
+  }
+
+  setTime() {
+    // Get time varaibles.
+    const seconds = this.timeNow.getSeconds()
+    const mins = this.timeNow.getMinutes()
+    const hour = this.timeNow.getMinutes()
+
+    // Convert seconds to degrees.
+    const secondsDegrees = ((seconds / 60) * 360)
+    const minsDegrees = (((mins / 60) * 360) + 90)
+    const hourDegrees = (((hour / 12) * 360) + 90)
+
+    // Set the degree to the clock hand.
+    this.seconds.style.transform = `rotate(${secondsDegrees}deg)`
+    this.hour.style.transform = `rotate(${minsDegrees}deg)`
+    this.mins.style.transform = `rotate(${hourDegrees}deg)`
+  }
+
+  intilize() {
+    // Create and inject the clock html.
+    this.createClock()
+    // Set interval update method.
+    setInterval(() => {
+      // Get current time
+      this.timeNow = new Date()
+      // Set the current time
+      this.setTime()
+      // Update every second.
+    }, 1000)
+  }
+}
+
+// instantiate the clock and intilize it.
+// const clock = new Clock('.target').intilize()
+
+for (let i = 0; i < 6; i++) {
+  new Clock('.target').intilize()
+}
